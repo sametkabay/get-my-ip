5888411eca7039de0bf918052a8569f08d69e653
Samet Kabay <samet.kabay@demirorenteknoloji.com>
2020-03-10 13:55:43 +0300

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IpService.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private IHttpContextAccessor _accessor;
+        public HomeController(IHttpContextAccessor accessor)
+        {
+            _accessor = accessor;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public ActionResult<string> Index()
+        {
+            string ip = _accessor.HttpContext.Connection.RemoteIpAddress.ToString();
+            return ip;
+        }
+    }
+}
